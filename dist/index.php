3b22efaeab03800dec1f48aceea4db20629085ee
Caio Alexandre <caioalexandre.br@hotmail.com>
2018-01-15 18:31:03 -0300

    Initial Commit

---
+++ dist/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dribbble API</title>
+
+    <script src="/dist/libs/angular/angular.js"></script>
+	<script src="/dist/libs/angular-route/angular-route.js"></script>
+	<script src="/dist/libs/angular-resource/angular-resource.js"></script>
+	<script src="/dist/libs/angular-sanitize/angular-sanitize.js"></script>
+    <script src="/dist/libs/angular-cookies/angular-cookies.js"></script>
+    
+    <link rel="stylesheet" href="/dist/style.css">
+
+</head>
+<body>
+
+Olá Mundo
+    
+</body>
+</html>
---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber'),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload,
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
+ 
+//Concat all JS
+gulp.task('concatJs', function() {
+  gulp.src('./dist/js/main.js')
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest('./dist/js/'));
+});
+
+// browser-sync task for starting the server.
+gulp.task('browserSync', function() {
+    //watch files
+    var files = [
+    './dist/style.css',
+    './dist/*.php',
+    './dist/js/*.js'
+    ];
+ 
+    //initialize browsersync
+    browserSync.init(files, {
+    //browsersync with a php server
+    proxy: "dribbble-api.dev",
+    notify: false
+    });
+});
+
+gulp.task('sass', function() {
+  gulp.src('./dist/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(plumber({
+      handleError: function (err) {
+        console.log(err)
+        this.emit('end')
+      }
+    }))
+    .pipe(sass())
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('default', ['browserSync', 'concatJs', 'sass'], function(){
+  gulp.watch('./dist/**/*.scss', ['sass']);
+  // Other watchers
+})
